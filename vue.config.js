c3d70e7219032e4690e8b1678ffa20b378a28415
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2024-06-26 22:12:51 +0700

    update server_type to https

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const fs = require("fs");
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    server: {
+      type: "https",
+      options: {
+        key: fs.readFileSync("./localhost-key.pem"),
+        cert: fs.readFileSync("./localhost.pem"),
+      },
+    },
+    port: 3000,
+    allowedHosts: "all", // Mengizinkan semua host
+    client: {
+      webSocketURL: "wss://localhost:3000/ws",
+    },
+    webSocketServer: "ws",
+  },
+});
