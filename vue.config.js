28e6fb1d01fb970f549287470f9b88b4906615c5
AnugrahSA <100453661+AnugrahSA@users.noreply.github.com>
2024-06-27 15:42:48 +0700

    new update https

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,4 @@
-const fs = require("fs");
 const { defineConfig } = require("@vue/cli-service");
-
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    server: {
-      type: "https",
-      options: {
-        key: fs.readFileSync("./localhost-key.pem"),
-        cert: fs.readFileSync("./localhost.pem"),
-      },
-    },
-    port: 3000,
-    allowedHosts: "all", // Mengizinkan semua host
-    client: {
-      webSocketURL: "wss://localhost:3000/ws",
-    },
-    webSocketServer: "ws",
-  },
 });
